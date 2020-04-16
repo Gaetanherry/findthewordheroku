b3a5b90f71b0c6f06e07845fa845c64501540bec
Ocynox <gaetanherry@hotmail.fr>
2020-04-16 20:33:10 +0200

    added graphics + player managing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./App.css";
+
+var socket;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       response: false,
+      nbPlayers : 0,
+      totalPlayers : 0,
       endpoint: "http://127.0.0.1:4001"
     };
+    socket=socketIOClient(this.state.endpoint);
   }
 
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("FromAPI", data => this.setState({ response: data }));
+    socket.emit("newPlayer");
+    socket.on("nbPlayers", data => this.setState({ nbPlayers: data }));
+    socket.on("totalPlayers", data => this.setState({ totalPlayers: data }));
+  }
+
+  addPlayer(num) {
+    socket.emit("addPlayer",num);
   }
 
   render() {
     const { response } = this.state;
     return (
-        <div style={{ textAlign: "center" }}>
+        <div id="container">
+          <h1>Trouve le mot, ou Mr. White !</h1>
+          <p>Nombre de Joueurs : {this.state.nbPlayers} / {this.state.totalPlayers} 
+          <button className="buttonDiv" onClick={() => this.addPlayer(1)}><i className="fas fa-plus fa-1x"></i></button> 
+          <button className="buttonDiv" onClick={() => this.addPlayer(-1)}><i className="fas fa-minus fa-1x"></i></button></p>
+          
           {response
               ? <p>
                 The current time is : {response}
